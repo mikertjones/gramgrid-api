d89aa442122ee46d85a30cec213f4e0f8266a5f0
mike <nmike.jones@martyj.co.uk>
2025-08-12 18:41:24 +0100

    removed env config

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-require('dotenv').config();
 
+// Only load dotenv in development
+/*if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+*/
 const express = require('express');
 const Database = require('better-sqlite3');
 const cors = require('cors');
